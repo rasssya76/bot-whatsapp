61bd418c5806d2f2d4cbd736ad64a94a9b6a89a4
I`am Caliph <92326690+Caliph91@users.noreply.github.com>
2021-10-30 15:19:11 +0700

    Update detect.js

--- message/detect.js
+++ message/detect.js
@@ -26,18 +26,21 @@ case 'add':
 if (!welcome.includes(json.jid)) return
 if (json.participants.includes(caliph.user.jid)) return caliph.reply(json.jid, `Hai,, Saya adalah *${caliph.user.name}*\n\nKetik ${prefix}menu untuk menampilkan menu`)
 for (let i of json.participants) {
-getpp = await caliph.getProfilePicture(i).catch(e => 'https://storage.caliph71.xyz/img/itsuki.jpg')
+getpp = await caliph.getProfilePicture(i).catch(e => 'https://storage.caliph71.xyz/img/404.jpg')
+gcicon = await caliph.getProfilePicture(json.jid).catch(e => 'https://storage.caliph71.xyz/img/404.jpg')
 caption = toformat(mess.group.welcome, i)
-var canvas = `https://api.caliph71.xyz/welcome?apikey=beta&name=${encodeURIComponent(caliph.getName(i))}&mem=${meta.participants.length}&gcname=${encodeURIComponent(meta.subject)}&picurl=${encodeURIComponent(getpp)}&bgurl=https://storage.caliph71.xyz/img/bg2.jpg`
+username = caliph.getName(i)
+var canvas = global.API('caliphAPI', '/api/welcome', { username, groupname: meta.subject, groupicon: gcicon, membercount: meta.participants.length, profile: getpp, background: 'https://storage.caliph71.xyz/img/bg2.jpg' }, 'apikey')
 caliph.sendMessage(json.jid, { url: canvas }, 'imageMessage', { caption, contextInfo: { mentionedJid: caliph.parseMention(caption)}})
 }
 break
 case 'remove': 
 if (!left.includes(json.jid)) return
-for (let i of json.participants) {
-getpp = await caliph.getProfilePicture(i).catch(e => 'https://storage.caliph71.xyz/img/itsuki.jpg')
-caption = toformat(mess.group.bye, i)
-var canvas = `https://api.caliph71.xyz/goodbye?apikey=beta&name=${encodeURIComponent(caliph.getName(i))}&mem=${meta.participants.length}&gcname=${encodeURIComponent(meta.subject)}&picurl=${encodeURIComponent(getpp)}&bgurl=https://storage.caliph71.xyz/img/bg2.jpg`
+getpp = await caliph.getProfilePicture(i).catch(e => 'https://storage.caliph71.xyz/img/404.jpg')
+gcicon = await caliph.getProfilePicture(json.jid).catch(e => 'https://storage.caliph71.xyz/img/404.jpg')
+caption = toformat(mess.group.welcome, i)
+username = caliph.getName(i)
+var canvas = global.API('caliphAPI', '/api/goodbye', { username, groupname: meta.subject, groupicon: gcicon, membercount: meta.participants.length, profile: getpp, background: 'https://storage.caliph71.xyz/img/bg2.jpg' }, 'apikey')
 caliph.sendMessage(json.jid, { url: canvas }, 'imageMessage', { caption, contextInfo: { mentionedJid: caliph.parseMention(caption)}})
 }
 break
